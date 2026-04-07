9af6a88c583916e1833e4cd4d815f9835b72cb2a
kwoktung <onlyif2009@gmail.com>
2026-04-07 18:44:14 +0800

    chore: add Cloudflare Workers deployment support

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
     devtools(),
+     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
